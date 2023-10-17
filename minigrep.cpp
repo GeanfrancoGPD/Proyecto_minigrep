105dd42fad684a29ebaf29acb69016dfdfe20778
Geanfranco <geanfranco1212@gmail.com>
2023-10-16 23:13:45 -0400

    feat:Busqueda de archivo, marca de las palabras y contador

--- minigrep.cpp
+++ minigrep.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include<windows.h>
 
 using namespace std;
 
@@ -7,6 +8,10 @@ int main (){
     ifstream archivo;
     ofstream outfile;
     string value;
+    string palabra;
+    int cont = 0;
+
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
     archivo.open("../mi_carpeta/oracion.txt", ios::in);
 
@@ -17,9 +22,30 @@ int main (){
         cout << "Se encontro" << endl;
     }
 
+    cout << "Ing.la palabra:"<< endl;;
+    cin >>palabra;
+
+
     while(archivo.good()){
+    	
         archivo >> value;
-        cout << value << " ";
+        if (palabra == value){
+        	SetConsoleTextAttribute(h,5);
+        	cout <<value << " ";
+        		SetConsoleTextAttribute(h,7);
+                cont = cont + 1;
+		}else{
+			
+			
+			cout << value;
+			cout << " ";
+		}
+        
     }
+    
+    cout << " " << endl;
+    cout << "Se repitio la frase: " << cont << " Veces " << endl;
+
+
 
 }
