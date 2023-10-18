d2bda7f7c5e9fe7b74fd9a0b0d8aa66d9e7481d0
Geanfranco <geanfranco1212@gmail.com>
2023-10-17 23:06:15 -0400

    Feat: seleccion de colores

--- minigrep.cpp
+++ minigrep.cpp
@@ -10,6 +10,7 @@ int main (){
     string value;
     string palabra;
     int cont = 0;
+    int x = 0;
 
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -24,28 +25,70 @@ int main (){
 
     cout << "Ing.la palabra:"<< endl;;
     cin >>palabra;
-
+    cout << "De que color quieres que se marque la palabra?" << endl;
+    cout << "1-rojo" << endl;
+    cout << "2-Azul" << endl;
+    cout << "3-amarillo" << endl;
+    cout << "4-blanco" << endl;
+    cin >> x;
 
     while(archivo.good()){
     	
         archivo >> value;
-        if (palabra == value){
-        	SetConsoleTextAttribute(h,5);
-        	cout <<value << " ";
+        
+        switch(x){
+            case 1 : //rojo
+             if (palabra == value){
+        	  SetConsoleTextAttribute(h,4);
+        	 cout <<value << " ";
         		SetConsoleTextAttribute(h,7);
                 cont = cont + 1;
-		}else{
-			
-			
-			cout << value;
-			cout << " ";
-		}
+		    }else{
+			   cout << value;
+			   cout << " ";
+		    }
+            break;
+
+            case 2: // azul
+
+            if (palabra == value){
+        	  SetConsoleTextAttribute(h,9);
+        	 cout <<value << " ";
+        		SetConsoleTextAttribute(h,7);
+                cont = cont + 1;
+		    }else{
+			   cout << value;
+			   cout << " ";
+		    }
+            break;
+
+            case 3: // amarillo 
+
+            if (palabra == value){
+        	  SetConsoleTextAttribute(h,14);
+        	 cout <<value << " ";
+        		SetConsoleTextAttribute(h,7);
+                cont = cont + 1;
+		    }else{
+			   cout << value;
+			   cout << " ";
+		    }
+            break;
+
+            case 4: //blanco
+
+            if (palabra == value){
+        	  SetConsoleTextAttribute(h,7);
+        	 cout <<value << " ";
+        		SetConsoleTextAttribute(h,7);
+                cont = cont + 1;
+		    }else{
+			   cout << value;
+			   cout << " ";
+		    }
+        }
         
     }
-    
     cout << " " << endl;
     cout << "Se repitio la frase: " << cont << " Veces " << endl;
-
-
-
 }
